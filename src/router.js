e3996eb7ffd2cbe5e05c602c7fa2043b430946f5
LiWei <3109554662@qq.com>
2019-10-11 20:05:59 +0800

    '基础架构的搭建'

--- src/router.js
+++ src/router.js
@@ -1,23 +1,60 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+
+//引入这个home的一级路由
+import Home from './views/home/index.vue';
+//引入home路由下的二级路由
+import Center from './views/home/center.vue';
+import Cinemas from './views/home/cinemas.vue';
+import Films from './views/home/films.vue';
+
+//引入其他的一级路由
+import City from './views/city/index.vue';
+import FilmInfo from './views/film/index.vue';
+import Login from './views/login/index.vue';
+import Money from './views/money/index.vue';
 
 Vue.use(Router)
 
-export default new Router({
+let router =  new Router({
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      children:[
+        {
+          path:'center',
+          component:Center
+        },
+        {
+          path:'cinemas',
+          component:Cinemas
+        },
+        {
+          path:'films',
+          component:Films
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/city',
+      name: 'city',
+      component: City
+    },
+    {
+      path:'/film/:id',
+      component:FilmInfo
+    },
+    {
+      path:'/login',
+      component:Login
+    },
+    {
+      path:'/money',
+      component:Money
     }
   ]
 })
+
+export default router;
---
+++ src/views/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page-home">
+    <!-- 一级路由页面 首页 -->
+    <!-- 这里是用来存放二级路由的 -->
+    <router-view></router-view>
+
+    <ul>
+      <router-link tag="li" to='/films'>电影</router-link>
+      <router-link tag="li" to='/cinemas'>影院</router-link>
+      <router-link tag="li" to='/zixun'>资讯</router-link>
+      <router-link tag="li" to='/center'>我的</router-link>
+    </ul>
+  </div>
+</template>
